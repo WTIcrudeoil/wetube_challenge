168092a3be84c71c965d70d2c6b09f58d2eac1aa
WTIcrudeoil <64414591+WTIcrudeoil@users.noreply.github.com>
2021-01-27 23:20:25 +0900

    CH-2_21HTML
    make HTMLs and
    working with Fake DB

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos =[
+    {
+        id:135,
+        title:'Video awesome',
+        description:' This is sth I love',
+        views:300,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator:{
+            id:111199,
+            name:"Nicolas",
+            email:"Ncls@naver.com"
+        }
+    },
+    {
+        id:338,
+        title:'Video Super',
+        description:' This is sth I love',
+        views:200,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator:{
+            id:111122,
+            name:"Nicolas",
+            email:"Ncls@naver.com"
+        }
+    },
+    {
+        id:675,
+        title:'Video hate',
+        description:' This is sth I love',
+        views:100,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator:{
+            id:111145,
+            name:"Nicolas",
+            email:"Ncls@naver.com"
+        }
+    }
+]

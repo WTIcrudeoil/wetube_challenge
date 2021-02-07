7cc99397c1376656b60f984206af48ce8f359a97
WTIcrudeoil <64414591+WTIcrudeoil@users.noreply.github.com>
2021-02-07 22:47:50 +0900

    CH5-0_webpackAndStyling
    installing webpack and setting webpack.
    styling w/ scss for header

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ import routes from "./routes";
 import { localsMiddleware } from "./middlewares";
 const app = express();
 
-app.use(helmet());
-app.use(function(req, res, next) {
-    res.setHeader("Content-Security-Policy", "script-src 'self' https://archive.org");
-    return next();
-});
+app.use(helmet({contentSecurityPolicy:false,}));
+// app.use(function(req, res, next) {
+//     res.setHeader("Content-Security-Policy", "script-src 'self' https://archive.org");
+//     return next();
+// });
 app.set('view engine',"pug");
 app.use("/uploads",express.static("uploads"));
+app.use("/static",express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
